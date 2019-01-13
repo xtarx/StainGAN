0871a6b7329d4901c2ae3706e2c699de8ee11568
XtarX <xtarx0@gmail.com>
2019-01-13 14:48:10 +0100

    add data folder

---
+++ data/slice_image.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.misc import imsave
+from scipy import ndimage
+from scipy import misc
+import scipy.misc
+import scipy
+import image_slicer
+from image_slicer import join
+from PIL import Image
+
+
+def name(self):
+    return 'UnalignedDataset'
